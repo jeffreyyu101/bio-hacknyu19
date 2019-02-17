29b2a23efb5a34c9f4476a462d5cd3bde86a9630
jeffreyyu101 <jeffreyyu101@gmail.com>
2019-02-17 13:58:59 -0500

    Create index.js

---
+++ templates/index.js
@@ -0,0 +1,17 @@
+body{
+  background-color: #4BC171;
+   color: white;
+   font-family: 'Montserrat', sans-serif;
+}
+
+a{
+  color: white;
+}
+a:hover{
+  color: white;
+  text-decoration: none
+}
+a:active{
+  color: white;
+  text-decoration: none
+}
